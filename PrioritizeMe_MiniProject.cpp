9256c9f3057b940cce87d94de94006b154c161f0
Talitha Reva Nabila <revanathaa@gmail.com>
2025-11-20 00:33:58 +0700

    Update PrioritizeMe_MiniProject.cpp

--- PrioritizeMe_MiniProject.cpp
+++ PrioritizeMe_MiniProject.cpp
@@ -15,29 +15,29 @@ using namespace std;
 
 // Fungsi untuk menghitung skor prioritas
 int hitungSkor (int deadline, int kesulitan, int kepentingan, int dampak){
-    return (6-deadline) + kesulitan + kepentingan + dampak;
+    return (6-deadline) + kesulitan + kepentingan + dampak;    //Deadline kecil jadi skor besar + faktor lain (kesulitan, kepentiungan, dampak)
 }
 // Fungsi input tugas
 void inputTugas(string namaTugas[], int deadline[], int kesulitan[], int kepentingan[], int dampak[], int skor[], int jumlahTugas){
-    for (int i = 0; i < jumlahTugas; i++){
-        cout << "\nMasukan nama tugas ke-" << i+1 << ":  ";
-        getline(cin, namaTugas[i]) ;
+    for (int i = 0; i < jumlahTugas; i++){                        //Loop untuk input setiap tugas
+        cout << "\nMasukan nama tugas ke-" << i+1 << ":  ";       //Input nama tugas
+        getline(cin, namaTugas[i]) ;                              //getline agar bisa input spasi dalam nama, bisa input lebih dari 1 kata
 
         cout << "Masukkan deadline (1-5, 1 = paling dekat): ";
-        cin >> deadline[i];
+        cin >> deadline[i];                                       //Input deadline, skala 1-5, 1 paling penting (prioritas)
 
         cout << "Masukkan tingkat kesulitan (1-5): ";
-        cin >> kesulitan[i];                                                
+        cin >> kesulitan[i];                                      //Input kesulitan dengan skala 1-5, 1 paling mudah          
 
         cout << "Masukkan tingkat kepentingan (1-5): ";
-        cin >> kepentingan[i];                                              
+        cin >> kepentingan[i];                                     //Input kepentingan tugas, skala 1-5, 5 paling penting          
 
-        cout << "Masukkan pengaruh terhadap nilai/prestasi (1-5): ";        
-        cin >> dampak[i];
+        cout << "Masukkan dampak terhadap nilai/prestasi (1-5): ";        
+        cin >> dampak[i];                                          //Input dampak tugas, skala 1-5, 1 dampak paling kecil
 
-        cin.ignore();    //digunakan untuk menghapus newline sebelum input nama berikutnya                                            
+        cin.ignore();                                              //digunakan untuk menghapus newline sebelum input nama berikutnya                                            
 
-        skor[i] = hitungSkor(deadline[i], kesulitan[i], kepentingan[i], dampak[i]);
+        skor[i] = hitungSkor(deadline[i], kesulitan[i], kepentingan[i], dampak[i]);    //memanggil fungsi untuk menghitung skor prioritas
     }
 }
 
@@ -59,9 +59,9 @@ void tampilkanTugas(string namaTugas[], int deadline[], int kesulitan[], int kep
 //Fungsi untuk mengurutkan urutan prioritas (menghasilkan indeks urutan)
 void urutkanPrioritas(int skor[], int jumlahTugas, int urutan[]) {
     //isi urut awal = 0,1,2,...
-for (int i=0; i<jumlahTugas; i++) {
-    urutan[i] = i;
-}
+    for (int i=0; i<jumlahTugas; i++) {
+        urutan[i] = i;
+    }
 // bubble sort indexs berdasarkan skor
     for(int i = 0; i < jumlahTugas - 1; i++){
         for(int j = 0; j < jumlahTugas - i - 1; j++){
@@ -73,21 +73,23 @@ for (int i=0; i<jumlahTugas; i++) {
        }
     }
 }
+
 int main() {
-int jumlah Tugas;
-cout << "Masukkan jumlah tugas: ";
-cin >> jumlahTugas;                                       // menginputkan jumlah tugas
-cin.ignore();                                            // bersihkan newline setelah angka
+    int jumlah Tugas;
+    cout << "Masukkan jumlah tugas: ";
+    cin >> jumlahTugas;                                       // menginputkan jumlah tugas
+    cin.ignore();                                            // bersihkan newline setelah angka
 
-string namaTugas [jumlahTugas];
-int deadline[jumlahTugas], kesulitan [jumlahTugas], kepentingan [jumlahTugas], dampak [jumlah Tugas], skor [jumlah Tugas];
-int urutan[jumlahTugas];                               // array indeks urut prioritas
+    string namaTugas [jumlahTugas];
+    int deadline[jumlahTugas], kesulitan [jumlahTugas], kepentingan [jumlahTugas], dampak [jumlah Tugas], skor [jumlah Tugas];
+    int urutan[jumlahTugas];                               // array indeks urut prioritas
 
-input Tugas (namaTugas, deadline, kesulitan, kepentingan, dampak, skor, jumlah Tugas);            //memanggil fungsi input Tugas
+    input Tugas (namaTugas, deadline, kesulitan, kepentingan, dampak, skor, jumlah Tugas);            //memanggil fungsi input Tugas
 
-urutkanPrioritas (skor, jumlahTugas, urutan);                                                     //memanggil fungsi urutkanPrioritas
+    urutkanPrioritas (skor, jumlahTugas, urutan);                                                     //memanggil fungsi urutkanPrioritas
 
-tampilkanTugas (namaTugas, deadline, kesulitan, kepentingan, dampak, skor, jumlahTugas, urutan);   //memanggil fungsi tampilkanTugas
+    tampilkanTugas (namaTugas, deadline, kesulitan, kepentingan, dampak, skor, jumlahTugas, urutan);   //memanggil fungsi tampilkanTugas
 
-return 0;
+    return 0;
 }
+
